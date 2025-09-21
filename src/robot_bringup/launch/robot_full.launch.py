e64afe7e97a550296db49d1c9e9821512f3b4282
andrinwinzap <AndrinWinzap@proton.me>
2025-09-21 13:09:53 +0200

    fix usb ports

--- src/robot_bringup/launch/robot_full.launch.py
+++ src/robot_bringup/launch/robot_full.launch.py
@@ -70,11 +70,13 @@ def generate_launch_description():
             arguments=["serial", "--dev", dev, "-b", "921600", "-v4"]
         )
         for i, dev in enumerate([
+            "/dev/ttyUSB0",
+            "/dev/ttyUSB1",
+            "/dev/ttyUSB2",
             "/dev/ttyUSB3",
             "/dev/ttyUSB4",
-            "/dev/ttyUSB6",
-            "/dev/ttyUSB8",
-            "/dev/ttyUSB0"
+            "/dev/ttyUSB5",
+            "/dev/ttyUSB6"
         ])
     ]
 

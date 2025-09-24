d69cea4c026d4da151abf16aff7ed4909ba8d262
andrinwinzap <AndrinWinzap@proton.me>
2025-09-24 16:00:16 +0200

    add example

---
+++ examples/pick_and_place.py
@@ -0,0 +1,76 @@
+from robot_api import Robot
+import math
+import time
+
+robot = Robot()
+robot.set_fake_hardware(False)
+robot.set_debug_mode(False)
+
+robot.joint_space.speed = 0.1
+robot.cartesian_space.speed = 0.03  
+
+robot.tool_changer.attach_tool(robot.tools.gripper)
+
+robot.joint_space.move(
+    Robot.JointSpace.Point((
+    0.52403,
+    -0.32743,
+    -1.81164,
+    -0.00000,
+    -1.00258,
+    0.52403
+    ))
+)
+
+robot.cartesian_space.move(
+    Robot.CartesianSpace.Pose(
+        (0.2,0.5,0.1), 
+        (0,0,0)
+        )
+    )
+
+robot.tools.gripper.set_distance(0.05)
+time.sleep(1)
+
+robot.cartesian_space.move(
+    Robot.CartesianSpace.Pose(
+        (0.2,0.5,0.04), 
+        (0,0,0)
+        )
+    )
+
+robot.tools.gripper.set_distance(0.00)
+time.sleep(1)
+
+robot.cartesian_space.move(
+    Robot.CartesianSpace.Pose(
+        (0.2,0.5,0.1), 
+        (0,0,0)
+        )
+    )
+
+robot.cartesian_space.move(
+    Robot.CartesianSpace.Pose(
+        (0.2,0.6,0.1), 
+        (0,0,0)
+        )
+    )
+
+robot.cartesian_space.move(
+    Robot.CartesianSpace.Pose(
+        (0.2,0.6,0.04), 
+        (0,0,0)
+        )
+    )
+
+robot.tools.gripper.set_distance(0.05)
+time.sleep(1)
+
+robot.cartesian_space.move(
+    Robot.CartesianSpace.Pose(
+        (0.2,0.6,0.1), 
+        (0,0,0)
+        )
+    )
+
+robot.shutdown()

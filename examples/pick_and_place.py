ac7d621a52089a3b191e39270b61008feb5152b2
andrinwinzap <AndrinWinzap@proton.me>
2025-09-25 13:21:55 +0200

    improve pick and place script

--- examples/pick_and_place.py
+++ examples/pick_and_place.py
@@ -2,6 +2,19 @@ from robot_api import Robot
 import math
 import time
 
+########################################
+START_X = 0.25
+START_Y = 0.5
+
+END_X = 0.2
+END_Y = 0.6
+
+PICK_PLACE_HEIGHT = 0.03
+TRAVEL_HEIGHT = 0.1
+
+OBJECT_SIZE = 0.02
+########################################
+
 robot = Robot()
 robot.set_fake_hardware(False)
 robot.set_debug_mode(False)
@@ -13,7 +26,7 @@ robot.tool_changer.attach_tool(robot.tools.gripper)
 
 robot.cartesian_space.move(
     Robot.CartesianSpace.Pose(
-        (0.2,0.5,0.1), 
+        (START_X,START_Y,TRAVEL_HEIGHT), 
         (0,0,0)
         ),
         False
@@ -24,31 +37,31 @@ time.sleep(1)
 
 robot.cartesian_space.move(
     Robot.CartesianSpace.Pose(
-        (0.2,0.5,0.03), 
+        (START_X,START_Y,PICK_PLACE_HEIGHT), 
         (0,0,0)
         )
     )
 
-robot.tools.gripper.set_distance(0.00)
+robot.tools.gripper.set_distance(OBJECT_SIZE)
 time.sleep(1)
 
 robot.cartesian_space.move(
     Robot.CartesianSpace.Pose(
-        (0.2,0.5,0.1), 
+        (START_X,START_Y,TRAVEL_HEIGHT), 
         (0,0,0)
         )
     )
 
 robot.cartesian_space.move(
     Robot.CartesianSpace.Pose(
-        (0.2,0.6,0.1), 
+        (END_X,END_Y,TRAVEL_HEIGHT), 
         (0,0,0)
         )
     )
 
 robot.cartesian_space.move(
     Robot.CartesianSpace.Pose(
-        (0.2,0.6,0.03), 
+        (END_X,END_Y,PICK_PLACE_HEIGHT), 
         (0,0,0)
         )
     )
@@ -58,7 +71,7 @@ time.sleep(1)
 
 robot.cartesian_space.move(
     Robot.CartesianSpace.Pose(
-        (0.2,0.6,0.1), 
+        (END_X,END_Y,TRAVEL_HEIGHT), 
         (0,0,0)
         )
     )
